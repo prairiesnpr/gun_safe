1cae4419b85ed2b431517fe5cc16931c4e79adc1
PrairieSnpr <midnryanmiller@gmail.com>
2024-07-27 12:38:52 -0400

    Yeah, that didn't work

--- zha/device_details.h
+++ zha/device_details.h
@@ -38,26 +38,26 @@ static attribute temp_attr[] = {{CURRENT_STATE, 0x00, 2, ZCL_INT16_T}};
 static attribute humid_attr[] = {{CURRENT_STATE, 0x00, 2, ZCL_UINT16_T}};
 
 static Cluster door_in_clusters[] = {
-    Cluster(BASIC_CLUSTER_ID, door_basic_attr, sizeof(door_basic_attr)),
-    Cluster(BINARY_INPUT_CLUSTER_ID, door_attr, sizeof(door_attr))};
+    Cluster(BASIC_CLUSTER_ID, door_basic_attr, sizeof(door_basic_attr) / sizeof(*door_basic_attr)),
+    Cluster(BINARY_INPUT_CLUSTER_ID, door_attr, sizeof(door_attr) / sizeof(*door_attr))};
 static Cluster vibration_in_clusters[] = {
-    Cluster(BASIC_CLUSTER_ID, vibration_basic_attr, sizeof(vibration_basic_attr)),
-    Cluster(IAS_ZONE_CLUSTER_ID, vibration_attr, sizeof(vibration_attr))};
+    Cluster(BASIC_CLUSTER_ID, vibration_basic_attr, sizeof(vibration_basic_attr) / sizeof(*vibration_basic_attr)),
+    Cluster(IAS_ZONE_CLUSTER_ID, vibration_attr, sizeof(vibration_attr) / sizeof(*vibration_attr))};
 static Cluster light_in_clusters[] = {
-    Cluster(BASIC_CLUSTER_ID, light_basic_attr, sizeof(light_basic_attr)),
-    Cluster(ON_OFF_CLUSTER_ID, light_bool_attr, sizeof(light_bool_attr)),
-    Cluster(LEVEL_CONTROL_CLUSTER_ID, light_level_attr, sizeof(light_level_attr)),
-    Cluster(COLOR_CLUSTER_ID, light_color_attr, sizeof(light_color_attr)),
+    Cluster(BASIC_CLUSTER_ID, light_basic_attr, sizeof(light_basic_attr) / sizeof(*light_basic_attr)),
+    Cluster(ON_OFF_CLUSTER_ID, light_bool_attr, sizeof(light_bool_attr) / sizeof(*light_bool_attr)),
+    Cluster(LEVEL_CONTROL_CLUSTER_ID, light_level_attr, sizeof(light_level_attr) / sizeof(*light_level_attr)),
+    Cluster(COLOR_CLUSTER_ID, light_color_attr, sizeof(light_color_attr) / sizeof(*light_color_attr)),
 };
 static Cluster t_in_clusters[] = {
-    Cluster(BASIC_CLUSTER_ID, temp_basic_attr, sizeof(temp_basic_attr)),
-    Cluster(TEMP_CLUSTER_ID, temp_attr, sizeof(temp_attr)),
-    Cluster(HUMIDITY_CLUSTER_ID, humid_attr, sizeof(humid_attr))};
+    Cluster(BASIC_CLUSTER_ID, temp_basic_attr, sizeof(temp_basic_attr) / sizeof(*temp_basic_attr)),
+    Cluster(TEMP_CLUSTER_ID, temp_attr, sizeof(temp_attr) / sizeof(*temp_attr)),
+    Cluster(HUMIDITY_CLUSTER_ID, humid_attr, sizeof(humid_attr) / sizeof(*humid_attr))};
 
 static Cluster out_clusters[] = {};
 static Endpoint ENDPOINTS[NUM_ENDPOINTS] = {
-    Endpoint(1, ON_OFF_SENSOR, door_in_clusters, out_clusters, sizeof(door_in_clusters), 0),
-    Endpoint(2, TEMPERATURE_SENSOR, t_in_clusters, out_clusters, sizeof(t_in_clusters), 0),
-    Endpoint(3, IAS_ZONE, vibration_in_clusters, out_clusters, sizeof(vibration_in_clusters), 0),
-    Endpoint(4, COLOR_LIGHT, light_in_clusters, out_clusters, sizeof(light_in_clusters), 0),
+    Endpoint(1, ON_OFF_SENSOR, door_in_clusters, out_clusters, sizeof(door_in_clusters) / sizeof(*door_in_clusters), 0),
+    Endpoint(2, TEMPERATURE_SENSOR, t_in_clusters, out_clusters, sizeof(t_in_clusters) / sizeof(*t_in_clusters), 0),
+    Endpoint(3, IAS_ZONE, vibration_in_clusters, out_clusters, sizeof(vibration_in_clusters) / sizeof(*vibration_in_clusters), 0),
+    Endpoint(4, COLOR_LIGHT, light_in_clusters, out_clusters, sizeof(light_in_clusters) / sizeof(*light_in_clusters), 0),
 };
